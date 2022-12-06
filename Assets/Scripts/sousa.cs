eccca2d1936c2af2fdbf16af4d9c2be7a94f07dc
TatugamiZero <hirokin2624@gmail.com>
2022-12-06 12:00:18 +0900

    aaa

--- Assets/Scripts/sousa.cs
+++ Assets/Scripts/sousa.cs
@@ -2,8 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ButtonTextChange : MonoBehaviour
+public class sousa : MonoBehaviour
 {
+    public GameObject image;
+    private bool flg = true;
+    public void OnClick() {
+        flg = !flg;
+        image.SetActive(flg);
+    }
     // Start is called before the first frame update
     void Start()
     {

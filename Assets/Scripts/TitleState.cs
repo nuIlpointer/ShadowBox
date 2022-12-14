a912fa001925ac247e1535d93781ee825537bd6a
nuIlpointer <npointa@gmail.com>
2022-12-14 16:36:08 +0900

    Create Title Scene
    これ１週間もないって間に合うんか？

---
+++ Assets/Scripts/TitleState.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public static class TitleData {
+    public static string ipAddress = "127.0.0.1";
+    public static int port = 11781;
+    public static bool isMultiPlay = false;
+}
+
+public class TitleState : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

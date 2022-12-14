54c20ff1b26464dde2ad710a16f0138410e2c40b
TatugamiZero <hirokin2624@gmail.com>
2022-12-14 13:53:23 +0900

    仮です

--- Assets/Scripts/kentiku.cs
+++ Assets/Scripts/kentiku.cs
@@ -1,20 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class kentiku : MonoBehaviour
 {
     // Start is called before the first frame update
     
     public GameObject image;
+    public GameObject ChangeButton;
+    
     private bool flg = true;
     public void Onclick() {
         flg = !flg;
         //image2.SetActive(flg);
         if (flg) {
             image.transform.position = new Vector3(image.transform.position.x, image.transform.position.y + 300, image.transform.position.z);
+            ChangeButton.GetComponentInChildren<Text>().text = "↑";
         } else {
             image.transform.position = new Vector3(image.transform.position.x, image.transform.position.y - 300, image.transform.position.z);
+            ChangeButton.GetComponentInChildren<Text>().text = "↓";
         }
         
     }

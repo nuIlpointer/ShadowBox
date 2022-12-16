2fc215f466c4fdb9030225396a10f1d1cd3cb779
nuIlpointer <npointa@gmail.com>
2022-12-16 09:20:44 +0900

    メニュー切り替えと画像タブ実装

--- Assets/Scripts/ToggleImage.cs
+++ Assets/Scripts/ToggleImage.cs
@@ -1,18 +1,45 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
-public class ToggleImage : MonoBehaviour
-{
+public class ToggleImage : MonoBehaviour {
+    [SerializeField] private Sprite[] instructImages;
+    [SerializeField] private GameObject imageTargetObj;
+    [SerializeField] private GameObject imageIndicatorObj;
+     
+    private TextMeshProUGUI indicator;
+    private Image image;
+    private int index = 0;
     // Start is called before the first frame update
-    void Start()
-    {
-        
+    void Start() {
+        image = imageTargetObj.GetComponent<Image>();
+        indicator = imageIndicatorObj.GetComponent<TextMeshProUGUI>();
+        Debug.Log($"{instructImages.Length} images loaded.");
+        image.sprite = instructImages[index];
+
+        indicator.text = GenerateIndicatorText(index, instructImages.Length);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    public void IncrementImage() {
+        if (index < instructImages.Length - 1) {
+            index++;
+            image.sprite = instructImages[index];
+            indicator.text = GenerateIndicatorText(index, instructImages.Length);
+        }
+    }
+
+    public void DecrementImage() {
+        if (index > 0) {
+            index--;
+            image.sprite = instructImages[index];
+            indicator.text = GenerateIndicatorText(index, instructImages.Length);
+        }
+    }
+
+    private string GenerateIndicatorText(int index, int length) {
+        var resText = "";
+        for (int i = 0; i < length; i++)
+            resText += (i == index ? "●" : "○");
+        return resText;
     }
 }
---
+++ Assets/Scripts/ToggleMenu.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToggleMenu : MonoBehaviour {
+    [SerializeField] private GameObject topObject;
+    
+    private GameObject currentObj;
+
+    // Start is called before the first frame update
+    void Start() {
+        currentObj = topObject;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    public void EnableObject(GameObject go) {
+        currentObj.SetActive(false);
+        go.SetActive(true);
+    }
+}

8df17a55f6aeda61a3f8bf7bdb54ee21ccfe0c71
magnet15s <mgns82shmy@gmail.com>
2022-12-19 11:43:28 +0900

    こみ

--- Assets/Scripts/CreateController.cs
+++ Assets/Scripts/CreateController.cs
@@ -7,7 +7,9 @@ public class CreateController : MonoBehaviour
 {
     public enum BrushTypes {
         sharp = 0,
-        bold = 1
+        bold = 1,
+        sharp_all_layer = 10,
+        bold_all_layer = 11
     }
     public WorldLoader worldLoader;
     public ShadowBoxClientWrapper wrapper;
@@ -18,6 +20,15 @@ public class CreateController : MonoBehaviour
 
     private int toBlock = -1;
 
+    private Vector2Int[] SHARP_MARKS = {
+        new Vector2Int(0, 0),
+    };
+
+    private Vector2Int[] BOLD_MARKS = { 
+        new Vector2Int(-1, 1), new Vector2Int( 0, 1), new Vector2Int( 1, 1),
+        new Vector2Int(-1, 0), new Vector2Int( 0, 0), new Vector2Int( 1, 0),                                
+        new Vector2Int(-1,-1), new Vector2Int( 0,-1), new Vector2Int( 1,-1), 
+    };
 
     /// <summary>
     /// useBlockとlineWidthの値に応じてブロックを設置します。
@@ -26,17 +37,47 @@ public class CreateController : MonoBehaviour
     /// <param name="y"></param>
     /// <param name="leyerNumber"></param>
     public void DrawBlock(int x, int y, int layerNumber) {
-        if(worldLoader.GetBlock(x, y, layerNumber) != useBlock) {
-            if (wrapper.IsConnectionActive()) {
+        Vector2Int[] marks;
+        //ブラシのマーク位置を決定
+        switch (lineWidth % 10) {
+            case 0:
+                marks = SHARP_MARKS;
+                break;
+            case 1:
+                marks = BOLD_MARKS;
+                break;
+        }
 
-                wrapper.SendBlockChange((ShadowBoxClientWrapper.BlockLayer)layerNumber, x, y, useBlock);
+        if(lineWidth / 10 == 1) {
+            for(int i = 1; i <= 4; i++) {
+                
 
-            } else {
+                if (worldLoader.GetBlock(x, y, i) != useBlock) {
+                    if (wrapper.IsConnectionActive()) {
 
-                worldLoader.BlockUpdate(useBlock, layerNumber, x, y); 
+                        wrapper.SendBlockChange((ShadowBoxClientWrapper.BlockLayer)i, x, y, useBlock);
 
+                    } else {
+
+                        worldLoader.BlockUpdate(useBlock, i, x, y);
+
+                    }
+                }
+            }
+        } else {
+            if (worldLoader.GetBlock(x, y, layerNumber) != useBlock) {
+                if (wrapper.IsConnectionActive()) {
+
+                    wrapper.SendBlockChange((ShadowBoxClientWrapper.BlockLayer)layerNumber, x, y, useBlock);
+
+                } else {
+
+                    worldLoader.BlockUpdate(useBlock, layerNumber, x, y);
+
+                }
             }
         }
+        
     }
 
     public void DeleteBlock(int x, int y, int layerNumber) {

87df92050726d7790bd6ecdfa3a74ed192d588fb
magnet15s <mgns82shmy@gmail.com>
2022-12-19 17:25:02 +0900

    BrushUi途中

--- Assets/Scripts/UseBrushViewer.cs
+++ Assets/Scripts/UseBrushViewer.cs
@@ -1,23 +1,50 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UseBrushViewer : MonoBehaviour
 {
     public CreateController cc;
     GameObject listUp;
     GameObject listDown;
+    GameObject nameView;
     private int selectBrush;
+    private int selectBArreyIdx;
+    int[] BrushArrey;
     // Start is called before the first frame update
     void Start()
     {
-        
+        BrushArrey = (int[])Enum.GetValues(typeof(CreateController.BrushTypes));
     }
 
     // Update is called once per frame
     void SetBrush(int o) {
-        selectBrush += o;
-        cc.lineWidth = selectBrush;
 
+        selectBArreyIdx += o;
+
+        //リスト上端の時
+        if (selectBArreyIdx <= 0) {
+            selectBArreyIdx = 0;
+            listUp.GetComponent<Image>().color = new Color(100, 100, 100);
+        } else {
+            listUp.GetComponent<Image>().color = new Color(255, 255, 255);
+        }
+        
+        //リスト下端の時
+        if (selectBArreyIdx >= BrushArrey.Length - 1) {
+            selectBArreyIdx = BrushArrey.Length - 1;
+            listDown.GetComponent<Image>().color = new Color(100, 100, 100);
+        } else {
+            listDown.GetComponent <Image>().color = new Color(255, 255, 255);
+        }
+        
+        selectBrush = BrushArrey[o];
+        //nameView.GetComponent<TextMeshPro>().text =;
+
+        cc.lineWidth = selectBrush;
+        
     }
 }

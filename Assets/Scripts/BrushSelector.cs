4151dedc38a1a588098139f8328f4183b238cbdc
magnet15s <mgns82shmy@gmail.com>
2022-12-20 00:34:42 +0900

    Revert "Revert "Merge branch 'MakeTerrain' into UI""
    This reverts commit 9ea85844a2cbecbf98e9331cd79da9d43bcfcec2.

--- Assets/Scripts/BrushSelector.cs
+++ Assets/Scripts/BrushSelector.cs
@@ -2,15 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BlockButton : MonoBehaviour
+public class BrushSelector : MonoBehaviour
 {
     public CreateController cc;
-    public int ccChange;
+
+
     // Start is called before the first frame update
-    
+    void Start()
+    {
+        
+    }
+
     // Update is called once per frame
-    
-    public void OnClick() {
-        cc.useBlock = ccChange;
+    void Update()
+    {
+        
     }
 }
